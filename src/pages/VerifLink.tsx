e6004fbb806c3dafba7c06f8ad5911ca188ff9ab
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-12-25 19:03:37 +0800

    Update VerifLink.tsx

--- src/pages/VerifLink.tsx
+++ src/pages/VerifLink.tsx
@@ -17,8 +17,7 @@ export const VerifLink: React.FC = () => {
   const adUrls = [
     'https://otieu.com/4/10055984',
     'https://smart-damage.com/HE9TFh',
-    'https://aviatorreproducesauciness.com/2082665',
-    'https://viiukuhe.com/dc/?blockID=388556'
+    'https://aviatorreproducesauciness.com/2082665'
   ];
 
   const getRandomAdUrl = () => {
